e254ce29f5b6f792f668eb2c13c3011b63b4bc28
GaoJunZhang <1228671674@qq.com>
2019-02-14 17:31:54 +0800

    132

--- pages/orderhistory/orderhistory.js
+++ pages/orderhistory/orderhistory.js
@@ -11,34 +11,27 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     let that = this
     var lastLanuage = app.globalData.lanuage
     // this.getContent(lastLanuage)
     app.getContent(that, lastLanuage)
     getOrder(that)
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  orderDetail: function(e) {
+    let that = this
+    app.fetchApis(that, 'e/order/' + e.currentTarget.dataset.id, null, 'GET', function(res) {
+      console.log(res)
+     
+    })
   }
 })
 
-var getOrder= function(that){
-  app.fetchApis(that, 'e/order/history', null, 'GET', function (res) {
+var getOrder = function(that) {
+  app.fetchApis(that, 'e/order/history', null, 'GET', function(res) {
     console.log(res)
     that.setData({
-      orders:res.data
+      orders: res.data
     })
   })
 }
